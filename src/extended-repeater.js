b98f83547091b282065107a16d6905ff34c87190
KirylZuyeu <kirylzuyeuimh@gmail.com>
2020-09-28 18:33:21 +0300

    feat: add solution of extend-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1
+  const separator = options.separator || '+'
+  const addition = String(options.addition !== undefined ? options.addition : '')
+  const additionRepeatTimes = options.additionRepeatTimes || 1
+  const additionSeparator = options.additionSeparator || '|'
+
+  let sumAddition = []
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    sumAddition.push(addition)
+  }
+  let resultAddition = sumAddition.join(additionSeparator)
+
+  let resultString = [];
+  for (let i = 0; i < repeatTimes; i++) {
+    resultString.push(`${str}${resultAddition}`)
+  }
+
+  return resultString.join(separator)
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,14 +1,14 @@
 module.exports = class DepthCalculator {
-    calculateDepth(arr) {
-        let depthMax = 1;
-        arr.forEach( item => {
-            if (Array.isArray(item)) {
-                let depth = this.calculateDepth(item) + 1;
-                if (depth > depthMax) {
-                    depthMax = depth;
-                }
-            }
-        });
-        return depthMax;
-}
+  calculateDepth(arr) {
+    let depthMax = 1;
+    arr.forEach(item => {
+      if (Array.isArray(item)) {
+        let depth = this.calculateDepth(item) + 1;
+        if (depth > depthMax) {
+          depthMax = depth;
+        }
+      }
+    });
+    return depthMax;
+  }
 };
